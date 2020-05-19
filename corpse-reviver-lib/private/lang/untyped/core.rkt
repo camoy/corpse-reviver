433fd52c3f3c1a1746eda78b1f96c475d366ccb1
Cameron Moy <camoy@cs.umd.edu>
2020-05-19 12:07:35 -0400

    Fix handling of require specs
    Previously we were not correctly handling specs (treating them
    all as unsafe). This deals with them more appropriately by looking
    at the spec expansion.

--- corpse-reviver-lib/private/lang/untyped/core.rkt
+++ corpse-reviver-lib/private/lang/untyped/core.rkt
@@ -15,6 +15,7 @@
 (require (for-syntax racket/base
                      racket/function
                      racket/require-transform
+                     racket/set
                      racket/syntax
                      racket/pretty
                      syntax/modresolve
@@ -28,7 +29,44 @@
 ;; macros
 
 (begin-for-syntax
-  (define unsafe-hash #f))
+  (define unsafe-hash #f)
+
+  ;; [Syntax Module-Path] → String
+  ;; Convert a module path (as syntax) to it's resolved absolute path (as a
+  ;; string).
+  (define (mod-path-stx->string mod-path-stx)
+    (~> mod-path-stx
+        syntax-e
+        resolve-module-path
+        path->string))
+
+  ;; Import → Boolean
+  ;; Returns if the import is safe according to the unsafe hash.
+  (define (safe-import? import)
+    (define src (mod-path-stx->string (import-src-mod-path import)))
+    (define src-id (import-src-sym import))
+    (and unsafe-hash
+         (hash-has-key? unsafe-hash src)
+         (not (member src-id (hash-ref unsafe-hash src)))))
+
+  ;; Syntax → Syntax
+  ;; Optimize a require spec by selectively importing safe bindings from the
+  ;; unsafe submodule.
+  (define (optimize-spec spec)
+    (define-values (imports _) (expand-import spec))
+    (define safe-imports (filter safe-import? imports))
+    (define exclude (map import-local-id safe-imports))
+    (define unsafe-mods
+      (set->list
+       (for/set ([safe-import (in-list safe-imports)])
+         (let ([mod (syntax->datum (import-src-mod-path safe-import))])
+           `(submod ,mod unsafe)))))
+    (define renames
+      (for/list ([safe-import (in-list safe-imports)])
+        (list (import-src-sym safe-import) (import-local-id safe-import))))
+    #`(combine-in
+       (except-in #,spec #,@exclude)
+       (only-in (combine-in #,@unsafe-mods) #,@renames))))
 
 ;; Registers the unsafe hash for use in the require form.
 (define-syntax (register-unsafe-hash! stx)
@@ -40,27 +78,12 @@
 ;; Rewrite require to import safe bindings from the uncontracted submodule.
 (define-syntax (-require stx)
   (syntax-parse stx
-    [(_ mods ...)
-     (define reqs
-       (for/list ([mod (in-list (attribute mods))])
-         (define mod-string
-           (and~>> mod
-                   syntax->datum
-                   (satisfies module-path?)
-                   resolve-module-path
-                   path->string))
-         (cond
-           [(and unsafe-hash (hash-has-key? unsafe-hash mod-string))
-            (define unsafes (hash-ref unsafe-hash mod-string))
-            (define unsafe-mod (format-symbol "~a/unsafe" (syntax->datum mod)))
-            (replace-context
-             stx
-             #`(begin
-                 (racket:require (only-in #,mod #,@unsafes))
-                 (racket:require (except-in (submod #,mod unsafe) #,@unsafes))))]
-           [else #`(require #,mod)])))
-     #`(begin #,@reqs)]))
+    [(_ spec ...)
+     (replace-context
+      stx
+      #`(racket:require #,@(map optimize-spec (syntax->list #'(spec ...)))))]))
 
+;; Provide, but also with an unsafe submodule.
 (define-syntax (-provide stx)
   (syntax-case stx ()
     [(_ spec ...)
@@ -79,5 +102,11 @@
    "add1: contract violation"
 
    #:x (dynamic-require "../../../test/langs/good-untyped.rkt" #f)
+   "adder: contract violation"
+
+   #:x (dynamic-require "../../../test/langs/bad-spec-untyped.rkt" #f)
+   "add1: contract violation"
+
+   #:x (dynamic-require "../../../test/langs/good-spec-untyped.rkt" #f)
    "adder: contract violation"
    ))
---
+++ corpse-reviver-lib/test/langs/good-spec-untyped.rkt
@@ -0,0 +1,13 @@
+#lang s-exp corpse-reviver/private/lang/untyped/base
+
+(require (for-syntax racket/base))
+
+(define-syntax (do-register-unsafe-hash _)
+  (define server-path
+    (path->string (path->complete-path "ty-server.rkt")))
+  (define h (hash server-path '(adder)))
+  #`(register-unsafe-hash! #,h))
+(do-register-unsafe-hash)
+
+(require (prefix-in pre: "ty-server.rkt"))
+(pre:adder "hi")
