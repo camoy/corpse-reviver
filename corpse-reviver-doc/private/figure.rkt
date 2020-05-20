3b34d6dc15af7aff9503d9f1e129dca6f1e9e841
Cameron Moy <camoy@cs.umd.edu>
2020-05-19 21:46:43 -0400

    More on figure

--- corpse-reviver-doc/private/figure.rkt
+++ corpse-reviver-doc/private/figure.rkt
@@ -12,6 +12,7 @@
          gtp-plot/configuration-info
          gtp-plot/performance-info
          gtp-plot/plot
+         gtp-plot/sample-info
          racket/hash
          racket/path
          racket/list
@@ -32,17 +33,19 @@
   (define runtimes (make-paths->hash "runtime" paths))
   (for/list ([(benchmark paths) (in-hash runtimes)])
     (displayln benchmark)
-    (overhead-plot (runtime-paths->performance-info benchmark paths))))
+    (samples-plot (runtime-paths->sample-info benchmark paths))
+    #;(overhead-plot (runtime-paths->performance-info benchmark paths))))
+
+(define (runtime-paths->sample-info benchmark paths)
+  (make-sample-info (runtime-paths->performance-info benchmark paths)
+                    (runtime-paths->samples paths)))
 
 (define (runtime-paths->performance-info benchmark paths)
   (define h (runtime-paths->hash paths))
   (define units (string-length (first (hash-keys h))))
   (define baseline-runtimes (hash-ref h (make-string units #\0)))
   (define typed-runtimes (hash-ref h (make-string units #\1)))
-  (define config-infos
-    (for/list ([(config runtimes) (in-hash h)])
-      ;; TODO: types
-      (configuration-info config 0 runtimes)))
+  (define config-infos (hash->configuration-infos h))
   (make-performance-info
    (string->symbol benchmark)
    #:src "."
@@ -53,10 +56,50 @@
    #:typed-runtime* typed-runtimes
    #:make-in-configurations (λ _ config-infos)))
 
+;; String → Natural
+;;
+(define (count-hi-bits cfg)
+  (for/sum ([c (in-string cfg)]
+            #:when (eq? c #\1))
+    1))
+
+;; [Listof Path] → [Listof [Listof Configuration-Info]]
+;; TODO
+(define (runtime-paths->samples paths)
+  (define run-hashes (append-map csv->hashes paths))
+  (define -hashes
+    (for/fold ([acc (hash)])
+              ([run-hash (in-list run-hashes)])
+      (define sample (string->number (hash-ref run-hash "sample")))
+      (hash-update acc sample (λ~>> (cons run-hash)) null)))
+  (define hashes (remove-baseline -hashes))
+  (for/list ([(k v) (in-hash hashes)])
+    (hash->configuration-infos (hash-collapse v))))
+
+;;
+;;
+(define (remove-baseline hashes)
+  (for/hash ([(k v) (in-hash hashes)]
+             #:when (not (andmap baseline? v)))
+    (values k v)))
+
+;;
+;;
+(define (baseline? h)
+  (define str (hash-ref h "config"))
+  (define n (string-length str))
+  (define hi (count-hi-bits str))
+  (or (= hi 0) (= hi n)))
+
 ;; [Listof Path] → [Hash String [Listof Natural]]
 ;;
 (define (runtime-paths->hash paths)
   (define run-hashes (append-map csv->hashes paths))
+  (hash-collapse run-hashes))
+
+;;
+;;
+(define (hash-collapse run-hashes)
   (define hashes
     (for/list ([run-hash (in-list run-hashes)])
       (match-define
@@ -64,6 +107,12 @@
         run-hash)
       (hash config (list (string->number real)))))
   (apply hash-union #:combine append hashes))
+
+;;
+;;
+(define (hash->configuration-infos h)
+  (for/list ([(config runtimes) (in-hash h)])
+    (configuration-info config (count-hi-bits config) runtimes)))
 
 ;; Path → [Listof [Hash Any Any]]
 ;; Convert a CSV file to a list of hashes with keys based on the header.
@@ -98,7 +147,8 @@
     (~>> path file-name-from-path path->string (regexp-match DATA-FILENAME-RX)))
   (and matches (cdr matches)))
 
-(main '("/home/camoy/wrk/corpse-reviver/corpse-reviver-doc/data/2020-05-19T11:05:10_sieve_runtime.csv"))
+(main '("/home/camoy/wrk/corpse-reviver/corpse-reviver-doc/data/2020-05-19T18:26:42_sieve_runtime.csv"))
+#;(main '("/home/camoy/wrk/corpse-reviver/corpse-reviver-doc/data/2020-05-19T11:10:54_fsm_runtime.csv"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; test
