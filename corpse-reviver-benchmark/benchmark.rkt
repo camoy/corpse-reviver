eb787a0c0b17b0ad348f7ecb9ac2415e1759bfb9
Cameron Moy <camoy@cs.umd.edu>
2020-06-01 18:22:27 -0400

    benchmark: Use places to parallelize

--- corpse-reviver-benchmark/benchmark.rkt
+++ corpse-reviver-benchmark/benchmark.rkt
@@ -11,45 +11,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; require
 
-(require (only-in rackunit require/expose)
-         corpse-reviver
-         corpse-reviver/private/compile
-         corpse-reviver/private/logging
-         data/queue
-         gtp-measure/private/configure
+(require data/queue
          gtp-measure/private/parse
-         gtp-measure/private/task
-         gtp-util
-         json
-         make-log-interceptor
-         mischief/for
+         racket/place
          racket/runtime-path
          racket/cmdline
-         racket/date
-         racket/exn
-         racket/hash
          racket/list
-         racket/match
-         racket/path
-         racket/port
-         racket/string
-         racket/syntax
          racket/system
+         racket/path
          threading
-         "util.rkt")
-
-(require/expose gtp-measure/private/task
-                (copy-configuration!
-                 pre-typed-untyped-subtask-tu-dir
-                 pre-typed-untyped-subtask-config-dir
-                 pre-typed-untyped-subtask-in-file*
-                 pre-typed-untyped-subtask-out-file*
-                 gtp-measure-task-dir
-                 format-target-tag
-                 in-pre-subtasks
-                 INPUT-EXTENSION
-                 make-gtp-measure-subtask
-                 write-lang!))
+         "private/task.rkt"
+         "private/util.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; consts
@@ -74,15 +46,7 @@
 ;; Path
 ;; Useful local paths.
 (define-runtime-path BENCHMARK-DIR "benchmarks")
-(define-runtime-path TYPED-RACKET-DIR "private/benchmark-typed-racket")
-
-;; [Hash Path Complete-Path]
-;; Proxy hash for patching Typed Racket. This is needed because running the
-;; benchmarks require some changes to Typed Racket.
-(define PROXY-HASH
-  (make-proxy-hash
-   #hash((typed-racket/base-env/prims-contract . "prims-contract.rkt"))
-   TYPED-RACKET-DIR))
+(define-runtime-path TASK "private/task.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; config
@@ -160,159 +124,33 @@
 ;; Benchmarks the given GTP typed/untyped targets.
 (define/contract (benchmark/scv-cr . targets)
   (->* () #:rest (listof valid-typed-untyped-target?) any)
-  (define config (make-config))
   (output-specs!)
+
+  ;; setup queue
+  (define queue-semaphore (make-semaphore 1))
+  (define target-queue (make-queue))
   (for ([target (in-list targets)])
-    (define task (init-task `((,target . ,kind:typed-untyped)) config))
-    (define benchmark (last-dir target))
-    (define analyses (make-queue))
-    (define runtimes (make-queue))
-    (init-untyped-typed-subtasks! task target)
-    (for*/fold ([n 0])
-               ([pre-subtask (in-list (in-pre-subtasks task))]
-                #:when #t
-                [subtask (in-list (pre-subtask->subtask* pre-subtask config))])
-      (set-config-sample! cfg n)
-      (set-config-benchmark! cfg benchmark)
-      (set-config-analyses! cfg analyses)
-      (set-config-runtimes! cfg runtimes)
-      (subtask-run! subtask)
-      (add1 n))
-    (output-json! benchmark 'analysis analyses)
-    (output-json! benchmark 'runtime runtimes)))
+    (enqueue! target-queue target))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; subtasks
-
-;; Task Path-String → Any
-;; Generates two new subtasks for the given task for a fully untyped and fully
-;; typed configuration. This is baseline information needed for plotting that
-;; may be otherwise unavailable (if sampling).
-(define (init-untyped-typed-subtasks! task target)
-  (define task-dir (gtp-measure-task-dir task))
-  (define base-filename (build-path task-dir (format-target-tag target 0)))
-  (define num-components (typed-untyped->num-components target))
-  (define (in-file subtask char)
-    (define filename
-      (~> base-filename
-          (path-add-extension (string-append "-" subtask))
-          (path-add-extension INPUT-EXTENSION)))
-    (with-output-to-file filename
-      (λ () (displayln (make-string num-components char)))))
-  (in-file "untyped" #\0)
-  (in-file "typed" #\1))
-
-;; Pre-Subtask Config → [Listof Subtask]
-;; Generates subtasks from a pre-subtask. Taken directly out of the `gtp-measure`
-;; package.
-(define (pre-subtask->subtask* pst config)
-  (define tu-dir (pre-typed-untyped-subtask-tu-dir pst))
-  (define config-dir (pre-typed-untyped-subtask-config-dir pst))
-  (define/for/lists (in-files out-files)
-    ([in-file (in-list (pre-typed-untyped-subtask-in-file* pst))]
-     [out-file (in-list (pre-typed-untyped-subtask-out-file* pst))]
-     #:when (not (file-exists? out-file)))
-    (values in-file out-file))
-  (typed-untyped->subtask* tu-dir config-dir in-files out-files config))
-
-;; Path Path [Listof Path] [Listof Path] Config → [Listof Subtask]
-;; Given some task information, generates a list of runnable subtasks. This is a
-;; modified version from `gtp-measure` that uses our custom runner thunk that
-;; keeps track of analysis and runtime information independently of the out
-;; files.
-(define (typed-untyped->subtask* tu-dir config-dir ins outs config)
-  (define entry
-    (~> config
-        (config-ref key:entry-point)
-        (build-path config-dir _)
-        path->string))
-  (for/list ([in (in-list ins)]
-             [out (in-list outs)])
-    (define (thunk [out-port (current-output-port)])
-      (write-lang! out-port "typed-untyped")
-      (with-input-from-file in
-        (λ ()
-          (for ([config-id (in-lines)]
-                [cfg-i (in-naturals 1)])
-            (copy-configuration! config-id tu-dir config-dir)
-            (define-values (config-in config-out) (make-pipe))
-            (define-values (analysis-times base-running-times)
-              (typed-untyped-run! entry config config-out config-id))
-            (close-output-port config-out)
-            (define raw-running-times (port->lines config-in))
-            (writeln (list config-id raw-running-times) out-port)
-            (define running-times
-              (map (λ~> time-string->list times->hash) raw-running-times))
-            (for ([time (in-list running-times)])
-              (enqueue! (config-runtimes cfg)
-                        (hash-union time
-                                    base-running-times
-                                    #:combine (λ (x _) x))))
-            (enqueue! (config-analyses cfg) analysis-times)
-            (when (empty? running-times)
-              (enqueue! (config-runtimes cfg) base-running-times))
-            (close-input-port config-in)))))
-    (make-gtp-measure-subtask out thunk config)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; typed-untyped-run!
-
-;; Path Config Path Natural → [Hash Symbol Any] [Hash Symbol Any]
-;; Given subtask information, populates hashs containing information from running
-;; that subtask. This is for both analysis and run-time.
-(define (typed-untyped-run! entry config config-out config-id)
-  (define dir (path-only entry))
-  (define targets (filter relevant-target? (directory-list dir #:build? dir)))
-  (define base `((benchmark . ,(config-benchmark cfg))
-                 (config . ,config-id)
-                 (sample . ,(config-sample cfg))
-                 (error . #f)))
-  (define analysis-times (make-hash base))
-  (define running-times
-    (make-hash (append base '((real . 0) (cpu . 0) (gc . 0)))))
-  (parameterize ([current-output-port config-out]
-                 [current-directory dir])
-    (when (typed-untyped-compile! targets analysis-times)
-      (typed-untyped-execute! entry config running-times)))
-  (values analysis-times running-times))
-
-;; [Listof Path] [Hash Symbol Any] → Boolean
-;; Compiles the given targets and populates the analysis time hash. It returns
-;; whether the compilation was successful.
-(define (typed-untyped-compile! targets analysis-times)
-  (define interceptor (make-log-interceptor scv-cr-logger))
-  (define-values (_ logs)
-    (interceptor
-     (λ ()
-       (with-handlers ([exn:fail? (make-error-handler analysis-times)])
-         (apply compile-files/scv-cr (map path->string targets))))))
-  (define times (map read-message (hash-ref logs 'info)))
-  (hash-union! analysis-times
-               (sum-times 'compile times)
-               (sum-times 'expand times)
-               (sum-times 'analyze times)
-               (hash 'blame (and~>> times (findf (has-tag? 'blame)) cdr))
-               #:combine (λ (_ x) x))
-  (not (hash-ref analysis-times 'error)))
-
-;; Path Config → Any
-;; Runs the program and populates the run-time hash with failure information.
-;; Notice that the run-time hash has no performance data yet. This happens in
-;; `typed-untyped->subtask*` instead.
-(define (typed-untyped-execute! entry config running-times)
-  (define resolved-entry (make-resolved-module-path (string->path entry)))
-  (define iterations (config-ref config key:iterations))
-  (for ([_ (in-range iterations)])
-    ;; HACK: Remove this (and all of private/benchmark-typed-racket) once TR
-    ;; #837 is resolved.
-    (with-patched-typed-racket
-      (λ ()
-        (with-handlers ([exn:fail? (make-error-handler running-times)])
-          (dynamic-require resolved-entry #f)))
-      PROXY-HASH)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; output
+  ;; run consumer threads
+  (define thds
+    (for/list ([_ (in-range (processor-count))])
+      (thread
+       (λ ()
+         (let go ()
+           (define maybe-target
+             (call-with-semaphore
+              queue-semaphore
+              (λ () (and (non-empty-queue? target-queue)
+                         (dequeue! target-queue)))))
+           (when maybe-target
+             (define pl (dynamic-place TASK 'place-benchmark))
+             (place-channel-put pl maybe-target)
+             (place-channel-put pl (struct->vector cfg))
+             (place-wait pl)
+             (go)))))))
+  (for ([thd (in-list thds)])
+    (thread-wait thds)))
 
 ;; → Any
 ;; Outputs machine specification information (Unix only). Specifically data about
@@ -322,107 +160,3 @@
   (with-output-to-file (build-path (config-output-dir cfg) filename)
     (λ ()
       (system "cat /proc/cpuinfo /proc/meminfo"))))
-
-;; String Symbol Queue → Any
-;; Given a queue of data, output this to a timestamped CSV file.
-(define (output-json! benchmark key queue)
-  (define filename (format "~a_~a_~a.json" (iso-timestamp) benchmark key))
-  (with-output-to-file (build-path (config-output-dir cfg) filename)
-    (λ ()
-      (~> queue queue->list list->jsexpr write-json))))
-
-;; [Listof Any] → JSExpr
-;; Given a list of values, convert this to a JSON convertible value.
-(define (list->jsexpr xs)
-  (define keys (hash-keys (first xs)))
-  (for/list ([x (in-list xs)])
-    (for/hash ([(k v) (in-hash x)])
-      (values k (->js v)))))
-
-;; Any → Any
-;; Returns a representation suitable for output to JSON. This requires mapping
-;; some symbols to strings.
-(define (->js x)
-  (cond
-    [(symbol? x) (symbol->string x)]
-    [(list? x) (map ->js x)]
-    [else x]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; util
-
-;; → Config
-;; Creates a configuration from the command line argument parameters.
-(define (make-config)
-  (init-config (hash key:iterations (config-iterations cfg)
-                     key:cutoff (config-cutoff cfg)
-                     key:sample-factor (config-sample-factor cfg)
-                     key:num-samples (config-num-samples cfg))))
-
-;; String → [List Natural Natural Natural]
-;; Parse a `time` string into a list of naturals.
-(define (time-string->list str)
-  (call-with-values
-   (λ () (time-string->values str))
-   list))
-
-;; Symbol [Listof [Cons Symbol Any]] → [Hash String Natural]
-;; Reduces down time lists that are tagged with `tag` by adding them together.
-;; Returns a hash containing the result.
-(define (sum-times tag times)
-  (define match? (has-tag? tag))
-  (define reduced-times
-    (for/fold ([acc (list 0 0 0)])
-              ([time (in-list times)])
-      (if (match? time)
-          (map + acc (cdr time))
-          acc)))
-  (times->hash reduced-times tag))
-
-;; [Listof [Cons Symbol Any]] {Symbol} → [Hash String Natural]
-;; Given a `time` list, returns a hash with that data.
-(define (times->hash times [key-prefix #f])
-  (define (symbol->key sym)
-    (if key-prefix (format-symbol "~a-~a" key-prefix sym) sym))
-  (match-define (list real cpu gc) times)
-  (hash (symbol->key 'real) real
-        (symbol->key 'cpu) cpu
-        (symbol->key 'gc) gc))
-
-;; Symbol → ([Cons Symbol Any] → Boolean)
-;; Returns if a cons cell has a certain tag.
-(define ((has-tag? tag) datum)
-  (eq? (car datum) tag))
-
-;; String → Any
-;; Returns the datum out of an SCV-CR logger message string.
-(define (read-message str)
-  (define log-rx #rx"^scv-cr: (.+)$")
-  (~>> str
-       (regexp-match log-rx)
-       second
-       open-input-string
-       read))
-
-;; Path → Symbol
-;; Returns the last directory of a path as a symbol.
-(define (last-dir path)
-  (define-values (_ name __) (split-path path))
-  (string->symbol (path->string name)))
-
-;; [Hash Symbol Any] → (Exception → Any)
-;; Make an exception handler that sets up an error entry in a hash.
-(define ((make-error-handler hash) e)
-  (hash-set! hash 'error (exn->string e)))
-
-;; Path → Boolean
-;; Returns if a target is relevant (i.e. is a Racket file and unless
-;; `current-no-skip?` is `#t`, ignores _ prefixed files).
-(define (relevant-target? target)
-  (and (path-has-extension? target #".rkt")
-       (or (config-no-skip? cfg) (not (underscore-prefixed? target)))))
-
-;; Path → Boolean
-;; Returns if the filename at the given path has an underscore prefix.
-(define (underscore-prefixed? target)
-  (~> target file-name-from-path path->string (string-prefix? "_")))
